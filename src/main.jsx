d16aab9f8b4e0bd96da7044fb88c6b8a0e48ec37
render217 <render21712@gmail.com>
2023-08-24 12:08:53 +0300

    minor change

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import GlobalContextProvider from "./context/GlobalContextProvider.jsx";
-import { SkeletonTheme } from "react-loading-skeleton";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <SkeletonTheme baseColor="#272a44">
-    <GlobalContextProvider>
-      <App />
-    </GlobalContextProvider>
-  </SkeletonTheme>
+
+  <GlobalContextProvider>
+    <App />
+  </GlobalContextProvider>
+
 );
